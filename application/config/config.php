5deb9e963bd5ca734ca5320bde2ce9e8969fe530
Anonymous <markndennis@hotmail.com>
2015-03-16 20:53:28 +0000

    changes to enable database to work in Codenvy

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://markndennis.net/marknden/CTCMBCDEV/';
+$config['base_url']	= '';
 
 /*
 |--------------------------------------------------------------------------
--- application/config/database.php
+++ application/config/database.php
@@ -48,7 +48,7 @@
 $active_group = 'default';
 $active_record = TRUE;
 
-$db['default']['hostname'] = 'localhost';
+$db['default']['hostname'] = '23.229.202.228';
 $db['default']['username'] = 'ctcmbc';
 $db['default']['password'] = 'ctcmBC001!';
 $db['default']['database'] = 'ctcmbc';
